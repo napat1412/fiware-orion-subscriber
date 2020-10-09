d4698edd61948e41a1da5ea4dc114d56bac1f06a
napat <napat@invoker.service.local>
2020-10-09 15:27:18 +0700

    Deleted the folder from the repository

--- tmp/array.js
+++
@@ -1,6 +0,0 @@
-var a = {}
-a['a'] = 1
-console.log(typeof a)
-if (a['a'] !== undefined) {
-  console.log(a['a'])
-}
--- tmp/test.js
+++
@@ -1,6 +0,0 @@
-var s = '42ss'
-console.log(s)
-if (! isNaN(Number(s))) {
-  console.log(typeof Number(s));
-}
-  
